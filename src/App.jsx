a448eaf950d936dac2ae7ece0beefa4821cff240
pchko <olga.pachowka@gmail.com>
2023-10-26 18:09:47 +0200

    todo final vesrion

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,84 @@
-import { useState } from "react"; // import nazwany
 import "./App.scss";
+import { useState } from "react";
+
+const getId = (todos) =>
+  todos.length === 0 ? 1 : Math.max(...todos.map((task) => task.id)) + 1;
 
 function App() {
-  // komponent
-  const [value, setValue] = useState(""); // stworzenie stanu komponentu, zwraca tablice bo ja destruktuuzjemy potrzebne zeby dane //byly persystentne
+  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  const [filters, setFilters] = useState("all");
 
-  function getNewId() {
-    if (todos.length === 0) {
-      return 1;
-    }
-    return Math.max(...todos.map((todo) => todo.id)) + 1;
-  }
+  // const getId = () => todos.length === 0 ? 1 : Math.max(...todos.map((task) => task.id)) + 1;
 
-  // todos = [{id: 1, name: 'a', status: 'active'}, {id: 2, name: 'b', status: 'active'}]
-  // map => [1, 2]
-  // ... => 1, 2
-  // Math.max(1, 2) => 2
-  // + 1 => 3
-
-  function handleValue(event) {
-    setValue(event.target.value); // uzywam fn do modyfikowania stanu //rerenderuje html // dzieje sie asynchronicznie
-  }
-
-  function handleAddTodo(event) {
-    if (event.key === "Enter") {
-      // z obiektu key pobiera czy to jest enter
+  const handleAddTodo = (evt) => {
+    if (evt.key === "Enter" && todo.trim().length >= 3) {
       setTodos([
-        ...todos,
         {
-          id: getNewId(),
-          name: value,
-          status: "active",
+          id: getId(todos),
+          status: "in progress",
+          title: todo,
         },
+        ...todos,
       ]);
-      setValue("");
+      setTodo("");
     }
-  }
-
-  function handleChangeStatus(todo) {
-    todo.status = todo.status === "active" ? "done" : "active";
+  };
+  const handleChangeStatus = (task) => {
+    task.status = task.status === "in progress" ? "done" : "in progress";
     setTodos([...todos]);
-  }
+  };
 
-  function handleDeleteTodo(task) {
-    setTodos(todos.filter((todo) => todo !== task));
-  }
+  const handleDeleteTodo = (todo) => {
+    setTodos(todos.filter((task) => task !== todo));
+  };
 
-  function handleDeleteAllDoneTodos() {
-    setTodos(todos.filter((todo) => todo.status !== "done"));
-  }
+  const handleDeleteDoneTasks = () => {
+    setTodos(todos.filter((task) => task.status !== "done"));
+  };
 
   return (
-    <div>
-      <h1>todos</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={handleValue} // dodanie zdarzenia na input // klamerki sluza do js
-        onKeyUp={handleAddTodo}
-      />
-      <ul className="todos">
-        {todos
-          .filter(
-            (todo) => (filters === "all" ? true : filters === todo.status) // wykonuje sie dla kazdego taska z osobna jeze
-          )
-          .map((todo) => (
-            <li className="todo" key={todo.id}>
+    <div className="todoapp">
+      <h1>todos🐈</h1>
+      <section className="todos">
+        <input
+          type="text"
+          className="todo-input"
+          value={todo}
+          placeholder="What needs to be done?"
+          onChange={(event) => setTodo(event.target.value)}
+          onKeyUp={handleAddTodo}
+        />
+        <ul className="todos-list">
+          {todos.map((task) => (
+            <li className="todos-item" key={task.id}>
               <span
-                className={todo.status === "active" ? "status" : "status done"}
-                onClick={() => handleChangeStatus(todo)} // wysoluje sie funkcja handle, todo to objekt pojednyczny z tablicy todos
+                className={
+                  task.status === "in progress" ? "status" : "status done"
+                }
+                onClick={() => handleChangeStatus(task)}
               ></span>
-
-              <span>{todo.name}</span>
+              <span>{task.title}</span>
               <button
                 className="btn-delete"
-                onClick={() => handleDeleteTodo(todo)}
+                onClick={() => handleDeleteTodo(task)}
               >
                 delete
               </button>
             </li>
           ))}
-      </ul>
-      <p>
-        {todos.filter((todo) => todo.status === "active").length} items left
-      </p>
-
-      <div>
-        <button
-          className={filters === "all" ? "current" : ""}
-          onClick={() => setFilters("all")}
-        >
-          All
-        </button>
-        <button
-          className={filters === "active" ? "current" : ""}
-          onClick={() => setFilters("active")}
-        >
-          Active
-        </button>
-        <button
-          className={filters === "done" ? "current" : ""}
-          onClick={() => setFilters("done")}
-        >
-          Done
-        </button>
-      </div>
-
-      {!!todos.filter((todo) => todo.status === "done").length && ( // zwrovi nowa tablice z taskami do done // conditional rendering
-        <button onClick={handleDeleteAllDoneTodos}>Clear completed</button>
-      )}
+        </ul>
+        <div className="box">
+          <p className="counter">
+            {todos.filter((task) => task.status === "in progress").length} items
+            left
+          </p>
+          {todos.some((task) => task.status === "done") && (
+            <button className="btn" onClick={() => handleDeleteDoneTasks}>
+              Clear{" "}
+            </button>
+          )}
+        </div>
+      </section>
     </div>
   );
 }
