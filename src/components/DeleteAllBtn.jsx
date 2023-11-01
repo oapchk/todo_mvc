e79c61f7b42aa043dd71dd8deb45437a002ea771
pchko <olga.pachowka@gmail.com>
2023-11-01 14:30:40 +0100

    spliting into components

---
+++ src/components/DeleteAllBtn.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+
+export function DeleteAllBtn({ handleDeleteDoneTasks }) {
+  return (
+    <button className="btn" onClick={handleDeleteDoneTasks}>
+      Clear completed
+    </button>
+  );
+}
+
+DeleteAllBtn.propTypes = {
+  handleDeleteDoneTasks: PropTypes.func.isRequired,
+};
+
+export default DeleteAllBtn;
---
+++ src/components/Headline.jsx
@@ -0,0 +1,5 @@
+function Headline() {
+  return <h1>todos 🐈</h1>;
+}
+
+export default Headline;
